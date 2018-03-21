6f0bb6ae6a65a1f78370aee611e9e98569748359
Pedro <pedro21fcp@gmail.com>
2018-03-21 12:21:40 +0000

    commit chunk

---
+++ src/chunks/Chunk.java
@@ -0,0 +1,69 @@
+package chunks;
+
+public class Chunk {
+	public static final int SIZE = 64000;
+	private static final long serialVersionUID = 1L;
+	private int repDegree;
+	private byte[] dados;
+	private String fileId;
+	private int chunkNo;
+
+	public Chunk(String fileId, int chunkNo, int repDegree, byte[] dados) {
+		this.fileId = fileId;
+		this.chunkNo = chunkNo;
+		this.repDegree = repDegree;
+		this.dados = dados;
+	}
+
+	public String getFileID() {
+		return this.fileId;
+	}
+	public int getChunkNo() {
+		return this.chunkNo;
+	}
+
+
+	public int getRepDegree() {
+		return this.repDegree;
+	}
+
+	public byte[] getDados() {
+		return this.dados;
+	}
+	
+	@Override
+	public String toString() {
+		return this.fileId + "-" + this.chunkNo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + this.chunkNo;
+		result = prime * result + ((fileId == null) ? 0 : this.fileId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Chunk other = (Chunk) obj;
+		if (this.chunkNo != other.chunkNo)
+			return false;
+		if (this.fileId == null) {
+			if (other.fileId != null)
+				return false;
+		} else if (!this.fileId.equals(other.fileId))
+			return false;
+		return true;
+	}
+	
+
+
+}
