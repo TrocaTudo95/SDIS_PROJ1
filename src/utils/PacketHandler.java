81e9e0b9c1a2fe68e7b8198f38216a8f9029215a
Pedro <pedro21fcp@gmail.com>
2018-03-29 11:38:30 +0100

    update

--- src/utils/PacketHandler.java
+++ src/utils/PacketHandler.java
@@ -1,10 +1,16 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
+import java.util.*;
 
 public class PacketHandler implements Runnable {
 
 	private DatagramPacket packet;
+	private String[] headerToken;
 	private String header;
 	private byte[] body;
 	
@@ -14,10 +20,71 @@ public class PacketHandler implements Runnable {
 	
 
 	
-	@Override
 	public void run() {
+		
+		this.headerToken = HeaderExtractor();
+		this.body = BodyExtractor();
+		if(this.headerToken[0] == "PUTCHUNK") {
+			// PUTCHUNK Handler
+		}else if(this.headerToken[0] == "STORED") {
+			//STORED Handler
+		}else if(this.headerToken[0] == "GETCHUNK") {
+			System.out.println("GETCHUNK");
+			
+		}else if(this.headerToken[0] == "CHUNK") {
+			System.out.println("CHUNK");
+			
+		}else if(this.headerToken[0] == "DELETE") {
+			System.out.println("DELETE");
+			
+		}else if(this.headerToken[0] == "REMOVED") {
+			System.out.println("REMOVED");
+			
+		}
 		
 		
 	}
+	
+	public String[] HeaderExtractor() {
+		ByteArrayInputStream stream = new ByteArrayInputStream(packet.getData());
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(stream));
+
+		try {
+			this.header = reader.readLine();
+			System.out.println(this.header);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return this.header.split("[ ]+");
+	}
+	
+	public byte[] BodyExtractor() {
+
+		ByteArrayInputStream stream = new ByteArrayInputStream(packet.getData());
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(stream));
+
+		String linha = "linha";
+		int SumLinhas = 0;
+		int numLinhas = 0;
+
+		while(!linha.isEmpty()) {
+			try {
+				linha = reader.readLine();
+				SumLinhas += linha.length();
+				numLinhas++;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		int BodyStarter = ("\r"+"\n").getBytes().length * SumLinhas + numLinhas;
+
+		return Arrays.copyOfRange(packet.getData(), BodyStarter,packet.getLength());
+	}
+
 
 }
--- src/utils/Services.java
+++ src/utils/Services.java
@@ -12,12 +12,42 @@ public class Services {
 	
 	void PUTCHUNK(Chunk chunk,Peer peer) {
 		String header = "PUTCHUNK" + " " + version + " " + peer.getID() + " " + chunk.getFileID() + " " + chunk.getChunkNo() + " " + chunk.getRepDegree() + " " + CRLF + CRLF;
-		// send Packet To MDB
+		// send To MDB
 	}
 
 	void STORED(Chunk chunk,Peer peer) {
 		String header = "STORED" + " " + version + " " + peer.getID() + " " + chunk.getFileID() + " " + chunk.getChunkNo() + " " + CRLF + CRLF ;
-		//send Packet to MC
+		//send to MC
+	}
+	
+	//Restore Headers
+	void GETCHUNK(Chunk chunk,Peer peer) {
+		
+		String header = "GETCHUNK" + " " + version + " " + peer.getID() + " " + chunk.getFileID() + " " + chunk.getChunkNo() + " " + CRLF + CRLF;
+
+		//Send To MC
+	}
+	
+	public void CHUNK(Chunk chunk, Peer peer) {
+		String header = "CHUNK" + " " + version + " " + peer.getID() + " "+ chunk.getFileID() + " " + chunk.getChunkNo() + " " + CRLF + CRLF;
+
+		//send to MDR
+	}
+	
+	//Delete Header
+	
+	public void DELETE(String fileID, Peer peer) {
+		String header = "DELETE" + " " + version + peer.getID() + " " + fileID + " " + CRLF + CRLF;
+
+		//sent to mc
+	}
+	
+	//Reclaim space header
+	
+	public void REMOVED(Chunk chunk, Peer peer) {
+		String header = "REMOVED" + " " + version + peer.getID() + " " + " " + chunk.getFileID() + " " + chunk.getChunkNo() + " " + CRLF + CRLF;
+
+		//send to mc
 	}
 	
 	
