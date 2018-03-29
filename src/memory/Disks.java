0117697e3c3fdf7a3b0eae645ee8c221a66be2f5
Pedro <pedro21fcp@gmail.com>
2018-03-29 15:26:09 +0100

    update

---
+++ src/memory/Disks.java
@@ -0,0 +1,55 @@
+package memory;
+
+import java.io.Serializable;
+
+public class Disks implements Serializable {
+	
+	private int capacity;
+	private int capacity_used;
+	private int freeSpace;
+	
+	public Disks(){
+		this.capacity = 10000000; //10MB
+		this.capacity_used = 0;
+		this.freeSpace = this.capacity - this.capacity_used;
+		
+	}
+	
+	public int getCapacity() {
+		return this.capacity;
+	}
+	public int getCapacityUsed() {
+		return this.capacity_used;
+	}
+	public int getFreeSpace() {
+		return this.freeSpace;
+	}
+	public void setCapacity(int Capacity) {
+		this.capacity = Capacity;
+		printDisk();
+	}
+	public void addCapacity(int bytes) {
+		this.capacity += bytes;
+		printDisk();
+	}
+	public void save(int bytes) {
+		if (bytes > this.freeSpace) {
+			System.out.println("Insuficient Space to Save");
+		} else {
+			this.capacity_used += bytes;
+			printDisk();
+		}
+	}
+	public void remove(int bytes) {
+		this.capacity_used -= bytes;
+		printDisk();
+	}
+	
+	public void printDisk() {
+		System.out.println("DISK");
+		System.out.println("Capacity: "+this.capacity);
+		System.out.println("Free Space: "+this.freeSpace);
+	}
+	
+
+}
