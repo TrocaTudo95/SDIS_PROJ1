5e4e5653b2baa6d2c8753da515afa25fe5a67115
Pedro <pedro21fcp@gmail.com>
2018-03-31 15:16:57 +0100

    update

--- src/BackupProtocol.java
+++ src/BackupProtocol.java
@@ -25,24 +25,27 @@ public class BackupProtocol implements Runnable{
         int chunkNo = 1;
 
         try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
-
-            int temp = 0;
-            do {
-                byte[] buffer = new byte[Chunk.SIZE];
-                temp = inputStream.read(buffer);
-                if(temp == -1) {
-                	byte[] emptyData = new byte[0];
-                	Chunk chunk= new Chunk(Functions.getHashedFileID(file),chunkNo,repDegree,emptyData);
-                BackupProtocol backup = new BackupProtocol(chunk,repDegree,Peer.getID());
-                backup.run();
-                }else {
-                		Chunk chunk= new Chunk(Functions.getHashedFileID(file),chunkNo,repDegree,buffer);
-                    BackupProtocol backup = new BackupProtocol(chunk,repDegree,Peer.getID());
-                    backup.run();
-                }
-                
-                chunkNo++;
-            } while (temp == Chunk.SIZE);
+        		int temp;
+            int nChunks = (int) (file.length()/Chunk.SIZE);
+            int lastChunk= (int) (file.length()%Chunk.SIZE);
+            if(nChunks > 0) {
+            		for(int i=0; i < nChunks; i++) {
+            				byte[] buffer = new byte[Chunk.SIZE];
+            				temp = inputStream.read(buffer);          		
+            				Chunk chunk= new Chunk(Functions.getHashedFileID(file),chunkNo,repDegree,buffer);
+            				BackupProtocol backup = new BackupProtocol(chunk,repDegree,Peer.getID());
+            				backup.run();
+ 
+            				chunkNo++;
+            			}
+            	
+            }
+            byte[] lastData = new byte[lastChunk];
+    			temp = inputStream.read(lastData);
+    			Chunk chunk= new Chunk(Functions.getHashedFileID(file),chunkNo,repDegree,lastData);
+    			BackupProtocol backup = new BackupProtocol(chunk,repDegree,Peer.getID());
+    			backup.run();
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
